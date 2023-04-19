5cbb5c15b295afb79e2357ca21745a59a024fcb1
♥ Mass ♥ <58617118+heyitsmass@users.noreply.github.com>
2023-04-19 07:57:18 -0700

    Update tester.py
    Refactor

--- tester.py
+++ tester.py
@@ -2,107 +2,134 @@ import asyncio
 from pathlib import Path 
 import subprocess 
 import os 
-import argparse 
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("phase", help="the current phase number.", type=int)
-
-sub_parsers = parser.add_subparsers(title="actions")  
-
-single_file_parser = sub_parsers.add_parser("single", help="Test a single file.") 
-multi_file_parser = sub_parsers.add_parser("multiple", help="Test multiple files.") 
-multi_file_parser.add_argument("-r", "--recheck", action="store_true", help="recheck the failed files outputted to failed.txt", required=False)
-single_file_parser.add_argument("filename", help="The current file being scanned.", type=str) 
-
-folder_group = multi_file_parser.add_mutually_exclusive_group() 
-
-folder_group.add_argument("-p", "--plus", action="store_true", help="test plus files", required=False) 
-folder_group.add_argument("-s", "--star", action="store_true", help="test star files", required=False)
-folder_group.add_argument("-a" ,"--all", action="store_true", help="test all files", required=False) 
-
-subfolder_group = multi_file_parser.add_mutually_exclusive_group()
-
-subfolder_group.add_argument("-b", "--bad", action="store_true", help="test bad files", required=False) 
-subfolder_group.add_argument("-g", "--good", action="store_true", help="test good files", required=False) 
-
-compile_options = parser.add_argument_group() 
-
-compile_options.add_argument("-c", "--compile", action="store_true", help="compile the program before running", required=False) 
-compile_options.add_argument("--silent", action="store_true", help="compile silently", required=False)
-
-parser.add_argument("-v", "--verbose", action="store_true", help="output diff info on failure.")
-args = parser.parse_args()
-
+from argparse import ArgumentParser
 import re 
 
 
-async def test(file, phase, verbose=False): 
-  project_dir = f"./Phase{phase}" 
+parser = ArgumentParser() 
+
+parser.add_argument("phase", 
+                    help="The current phase number.", 
+                    type=int, default=6) 
+
+subparsers = parser.add_subparsers(title="options") 
+
+singleFileParser = subparsers.add_parser("single", help="Test a single file.") 
+multiFileParser = subparsers.add_parser("multiple", help="Test multiple files.") 
+
+singleFileParser.add_argument("filename", 
+                              help="The current file being scanned.", 
+                              type=str)
+
+multiFileParser.add_argument("-r", "--recheck", 
+                             help="Recheck the failed files outputted to failed.txt", 
+                             action="store_true", required=False, default=False) 
+
+folderGroup = multiFileParser.add_mutually_exclusive_group()
+
+folderGroup.add_argument("-p", "--plus",
+                         help="Test plus files.", 
+                         action="store_true", required=False, default=False)
+folderGroup.add_argument("-s", "--star", 
+                         help="Test star files.", 
+                         action="store_true", required=False, default=False)
+folderGroup.add_argument("-a", "--all", 
+                         help="Test all files.",
+                         action="store_true", required=False, default=False)
+
+subFolderGroup = multiFileParser.add_mutually_exclusive_group() 
+
+subFolderGroup.add_argument("-b", "--bad",
+                            help="Test bad files.",
+                            action="store_true", required=False, default=False)
+subFolderGroup.add_argument("-g", "--good", 
+                            help="Test good files.", 
+                            action="store_true", required=False, default=True)
+
+compileOptions = parser.add_argument_group()
+
+compileOptions.add_argument("-c", "--compile",
+                            help="Compile the program before running.",
+                            action="store_true", required=False, default=False)
+compileOptions.add_argument("--silent", 
+                            help="Compile silently.",
+                            action="store_true", required=False, default=False)
+parser.add_argument("-v", "--verbose", 
+                    help="Output diff on failure.", 
+                    action="store_true", required=False, default=False)
+
+args = parser.parse_args()
+
+def readData(filename:str) -> list[str]: 
+  with open(filename, 'r') as file: 
+    return file.readlines() 
+
+def writeData(data:list, filename:str): 
+  with open(filename, 'w') as file: 
+    file.writelines(data) 
+
+
+async def test(file:Path, verbose:bool=False): 
+  projectDir = f"./Phase{args.phase}"
   print(f"\033[33mTesting {file.name}:\033[0m", end="")
-  with open('user', 'w') as user, open('ref', 'w') as ref: 
+  with open('user', 'w') as user, open('ref', 'w') as ref:
     subprocess.run(
-      ['./espressoc', f"../{file}"], cwd=project_dir, stdout=user
+      ['./espressoc', f"../{file}"], cwd=projectDir, stdout=user
     )
 
     subprocess.run(
-      ['./espressocr', f'../{file}'], cwd=project_dir, stdout=ref
+      ['./espressocr', f'../{file}'], cwd=projectDir, stdout=ref
     )
 
-    
-    diff = subprocess.run(["diff", "-I", ".*: '.*'", "user", "ref"], stdout=subprocess.PIPE, encoding='utf8').stdout 
-
+    diff = subprocess.run(["diff", "-I", ".*: '.*'", "user", "ref"], 
+                          stdout=subprocess.PIPE, encoding='utf8').stdout 
 
   if not diff: 
     print(f"\033[32m PASSED INITIAL CHECK\033[0m")
+
     try: 
-      err_data = None
-      with open('user', 'r') as ufile, open('ref', 'r') as rfile: 
-        err_data = ufile.read()
+      errData:str = None 
+      with open('user', 'r') as user, open('ref', 'r') as ref: 
+        errData = user.read() 
 
-        userFileData = re.split(r'-+\n', err_data)[2] 
-        refFileData = re.split(r'-+\n', rfile.read())[2]
+        userFile = re.split(r'-+\n', errData)[2]
+        refFile = re.split(r'-+\n', ref.read())[2]
 
+        userFiles = re.findall(r'\'((.+)[.](?:rj|j))\'', userFile)
+        refFiles = re.findall(r'\'((.+)[.](?:rj|j))\'', refFile)
 
-        userFiles = re.findall(r'\'((.+)[.](?:rj|j))\'', userFileData)
-        refFiles = re.findall(r'\'((.+)[.](?:rj|j))\'', refFileData)
-      
-      for i, (userFilename, userBasename) in enumerate(userFiles): 
-        refFilename, refBasename = refFiles[i]
+        for i, (userFileName, userBaseName) in enumerate(userFiles): 
+          refFileName, refBaseName = refFiles[i] 
 
-        if userBasename != refBasename:
-          raise Exception(f"{userFilename} does not match {refFilename}")
+          if userBaseName != refBaseName: 
+            raise NameError(f"{userFileName} does not match {refFileName}")
+          
+          print(f"\033[36mVerifying {userFileName}: \033[0m", end="")
 
-        print(f"\033[36mVerifying {userFilename}: \033[0m", end="")
-
-        diff = subprocess.run(
-          ["diff", userFilename, refFilename], 
-          cwd=project_dir, 
-          stdout=subprocess.PIPE
-        ).stdout 
-
-        if diff: 
-          raise Exception(f"{userFilename} does not match {refFilename}.") 
-        else: 
-          print(f"\033[32m VALID\033[0m")
-      
-    except Exception as e: 
+          diff = subprocess.run(["diff", userFileName, refFileName], 
+                                cwd=projectDir, 
+                                stdout=subprocess.PIPE).stdout 
+          if diff: 
+            raise RuntimeError(f"{userFileName} does not match {refFileName}.")
+          else:
+            print(f"\033[32m VALID\033[0m")
+            
+    except Exception as err: 
       print(f"\033[31mFILE VALIDATION FAILED \033[0m", end="")
-      print(f":{e}" if verbose or args.verbose else '')
+      print(f":{err}" if verbose or args.verbose else '')
 
-      if type(e) == IndexError:  
+      if type(err) == IndexError: 
         print(f"\033[31mERROR IN MAIN FILE: \033[0m")
-        print(err_data) 
+        print(errData) 
         return file 
-      
-      if verbose or args.verbose: 
+    
+      if verbose or args.verbose and type(err) != NameError: 
         subprocess.run(
-          ["diff", "-y", userFilename, refFilename], 
-          cwd=project_dir
+          ["diff", "-y", userFileName, refFileName], 
+          cwd=projectDir
         )
-      pass 
-      
 
+      pass 
   else: 
     print(f"\033[31m FAILED INITIAL CHECK\033[0m")
     if verbose or args.verbose: 
@@ -111,111 +138,102 @@ async def test(file, phase, verbose=False):
       )
     return file 
 
+def compile(): 
 
-        
-async def main(phase:int=1): 
-  tests_dir = f"./unit_tests/{phase}/" 
+  if args.silent: 
+    print("Silently Compiling...") 
+    subprocess.run(["ant"], stdout=subprocess.PIPE, cwd=f"./Phase{args.phase}")
+  else: 
+    subprocess.run(["ant"], cwd=f"./Phase{args.phase}")     
+
+  print("Compiled.")
+  
+async def main(): 
+  testsFolder = f"./unit_tests/{args.phase}/" 
+  failedFiles:list[str] = [] 
 
   options = { 
     "plus" : ["_Plus"],
     "star": ["_Star"], 
     "all": ["", "_Plus", "_Star"], 
     "bad": ["BadTests"], 
-    "good": ["GoodTests"]
+    "good": ["GoodTests"] 
   }
 
-  failed = [] 
-
-  parsedOptions = [key for key, value in args._get_kwargs()]
-
-  if 'compile' in parsedOptions and args.compile:   
-    if 'silent' in parsedOptions and args.silent: 
-      print("Silently compiling...")
-      subprocess.run(["ant"], stdout=subprocess.PIPE, cwd=f"./Phase{args.phase}")
-    else: 
-      subprocess.run(["ant"], cwd=f"./Phase{args.phase}")
-      
-    print("Compiled.")
-
-  if 'filename' not in parsedOptions: 
-    if 'recheck' in parsedOptions and args.recheck: 
-      print("Rechecking")
+  if 'filename' not in args: 
+    if 'recheck' in args and args.recheck: 
+      print("Rechecking") 
       try: 
-        with open('failed.txt', 'r') as failedFile: 
-          files = failedFile.readlines() 
 
-          for file in files: 
-            if file == '\n': continue 
-            ret = await test(Path(file[:-1]), phase) 
+        files = readData("failed.txt")
+        if args.compile: 
+          compile() 
+
+        for file in files: 
+          if file != '\n': 
+            ret = await test(Path(file[:-1])) 
 
             if ret: 
-              failed.append(f"./{file}\n")
+              failedFiles.append(f"./{file}\n") 
+        
+        writeData(failedFiles, "failed.txt") 
 
-        with open('failed.txt', 'w') as file: 
-          file.writelines(failed)  
+      except FileNotFoundError as err: 
+        print("Unable to find failed.txt, run initial tests first.") 
+        return 
+    else:
+      folders = [""]
+      subfolders = ["GoodTests", "BadTests"] 
 
-      except FileNotFoundError as e: 
-        print("Unable to find failed.txt, run initial tests first.")
-        pass 
+      for key, value in args._get_kwargs(): 
+        if key not in ['phase', 'compile', 'recheck', 'silent', 'verbose']: 
+          if value: 
+            if key in ["plus", "star", "all"]:
+              folders = options[key] 
+            else: 
+              subfolders = options[key] 
+     
+      if args.compile: 
+        compile() 
 
-      return
+      for folder in folders: 
+        for subfolder in subfolders: 
+          testDir = testsFolder + f'/Espresso{folder}/{subfolder}'
+          files = Path(testDir) 
+          print(f"\033[36mEspresso{folder}: \033[34m{subfolder}\033[0m")
+          for f in sorted(files.iterdir()): 
+            file = await test(f) 
+            if file: 
+              failedFiles.append(f"./{file}\n")
       
-    folders = [""]
-    subfolders = ["GoodTests", "BadTests"] 
-
-    for key, value in args._get_kwargs(): 
-      if key in ['phase', 'compile', 'recheck', 'silent', 'verbose']: continue 
-      if value: 
-        if key in ["plus", "star", "all"]: 
-          folders = options[key] 
-        else: 
-          subfolders = options[key] 
-  
-    for folder in folders:
-      for subfolder in subfolders: 
-        test_dir =  tests_dir + f'/Espresso{folder}/{subfolder}'
-
-        files = Path(test_dir)
-
-        print(f"\033[36mEspresso{folder}: \033[34m{subfolder}\033[0m")
-        for f in sorted(files.iterdir()): 
-          file = await test(f, phase) 
-
-          if file: 
-            failed.append(f"./{file}\n")
-    
-    with open('failed.txt', 'w') as file: 
-      file.writelines(failed) 
-
+      writeData(failedFiles, "failed.txt")
   else: 
-
-    file = None 
+    file = None
     if './unit_tests' in args.filename: 
       file = Path(args.filename) 
 
-
     if not file: 
-      file_loc = ""
+      fileLoc:str = "" 
 
-      for root, dirs, files in os.walk(tests_dir): 
+      for root, dirs, files in os.walk(testsFolder): 
         for file in files: 
           if file.lower() == args.filename: 
-            if file_loc: 
-              raise RuntimeError(f"Multiple file locations found for same file. Retry with exact filepath. \n\t{file_loc}, or \n\t{os.path.join(root, file)}")
-             
-            file_loc = os.path.join(root, file) 
+            if fileLoc: 
+              raise RuntimeError(
+                f"Multiple file locations found for same file. Retry with exact filepath." +
+                f"\n\t{fileLoc}, or \n\t{os.path.join(root, file)}")
 
+      if not fileLoc: 
+        raise RuntimeError("Unable to find file. (Invalid name?)")
       
-      if not file_loc: 
-        raise RuntimeError("Unable to find file.") 
+      if args.compile: 
+        compile() 
 
-      file = Path(file_loc) 
+      file = Path(fileLoc)
+
+      await test(file, True) 
+
+if __name__ == "__main__":
+  asyncio.run(main())
 
 
-    await test(file, phase, True) 
-
-if __name__ == "__main__": 
-
-  asyncio.run(main(args.phase)) 
-
-  
