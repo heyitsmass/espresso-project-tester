fdd0255293f7d9a621928abfd200b87bab667d43
♥ Mass ♥ <58617118+heyitsmass@users.noreply.github.com>
2023-04-24 19:22:21 -0700

    Update tester.py
    Added comments

--- tester.py
+++ tester.py
@@ -61,26 +61,46 @@ parser.add_argument("-v", "--verbose",
 args = parser.parse_args()
 
 def readData(filename:str): 
+
+  """Read the lines of a file and return them as an array.
+
+  Keyword arguments:
+    filename -- the file being read 
+  """
   with open(filename, 'r') as file: 
     return file.readlines() 
 
 def writeData(data:list, filename:str): 
+  """Write an array of data to a file.
+
+  Keyword arguments:
+    data -- list of data (strings) being written
+    filename -- The file being written to
+  """
   with open(filename, 'w') as file: 
     file.writelines(data) 
 
 
 async def test(file:Path, verbose:bool=False): 
-  projectDir = f"./Phase{args.phase}"
+  """Test a single file
+
+  Keyword arguments:
+    file -- the file being tested
+    verbose -- Determines verbosity of the output if args.verbose is not provided
+  """
+
+  projectDir = f"./Phase{args.phase}" #The project dir 
   print(f"\033[33mTesting {file.name}:\033[0m", end="")
   with open('user', 'w') as user, open('ref', 'w') as ref:
     subprocess.run(
-      ['./espressoc', f"../{file}"], cwd=projectDir, stdout=user
+      ['./espressoc', f"../{file}"], cwd=projectDir, stdout=user  #Test against the user compiler
     )
 
     subprocess.run(
-      ['./espressocr', f'../{file}'], cwd=projectDir, stdout=ref
+      ['./espressocr', f'../{file}'], cwd=projectDir, stdout=ref #Test against the ref compiler
     )
 
+    #Run the diff check, ignore file names for now, they're always non-matching
     diff = subprocess.run(["diff", "-I", ".*: '.*'", "user", "ref"], 
                           stdout=subprocess.PIPE, encoding='utf8').stdout 
 
@@ -90,22 +110,26 @@ async def test(file:Path, verbose:bool=False):
     try: 
       errData:str = None 
       with open('user', 'r') as user, open('ref', 'r') as ref: 
-        errData = user.read() 
+        # Store the file data incase an error is detected, it contains the message
+        errData = user.read()  
 
-        userFile = re.split(r'-+\n', errData)[2]
-        refFile = re.split(r'-+\n', ref.read())[2]
+        userFile = re.split(r'-+\n', errData)[2]  #Collect the filenames from the user output
+        refFile = re.split(r'-+\n', ref.read())[2]  #Collect the filenames from the ref ouput
 
-        userFiles = re.findall(r'\'((.+)[.](?:rj|j))\'', userFile)
-        refFiles = re.findall(r'\'((.+)[.](?:rj|j))\'', refFile)
+        #Seperate out the filenames and their base names
+        userFiles = re.findall(r'\'((.+)[.](?:rj|j))\'', userFile)  
+        refFiles = re.findall(r'\'((.+)[.](?:rj|j))\'', refFile)  
 
+        #Iterate over the filenames
         for i, (userFileName, userBaseName) in enumerate(userFiles): 
           refFileName, refBaseName = refFiles[i] 
 
-          if userBaseName != refBaseName: 
+          if userBaseName != refBaseName: #Ensure the file names match before comparing
             raise NameError(f"{userFileName} does not match {refFileName}")
           
           print(f"\033[36mVerifying {userFileName}: \033[0m", end="")
 
+          #Compare the user file and the reference file
           diff = subprocess.run(["diff", userFileName, refFileName], 
                                 cwd=projectDir, 
                                 stdout=subprocess.PIPE).stdout 
@@ -113,17 +137,20 @@ async def test(file:Path, verbose:bool=False):
             raise RuntimeError(f"{userFileName} does not match {refFileName}.")
           else:
             print(f"\033[32m VALID\033[0m")
-            
+    
     except Exception as err: 
+      #Caught an exception in checking the files
       print(f"\033[31mFILE VALIDATION FAILED \033[0m", end="")
       print(f":{err}" if verbose or args.verbose else '')
 
+      #We're missing filenames!?! Error in the main file
       if type(err) == IndexError: 
         print(f"\033[31mERROR IN MAIN FILE\033[0m")
         if verbose or args.verbose:
           print(f': {errData}') 
         return file 
-    
+
+      #We have file names but some file didn't validate.
       if verbose or args.verbose and type(err) != NameError: 
         subprocess.run(
           ["diff", "-y", userFileName, refFileName], 
@@ -132,17 +159,19 @@ async def test(file:Path, verbose:bool=False):
 
       pass 
   else: 
+    #If we failed the initial check, output the diff if requested.
     print(f"\033[31m FAILED INITIAL CHECK\033[0m")
     if verbose or args.verbose: 
       subprocess.run(
         ["diff", "-y", "-t", "user", "ref"]
       )
-    return file 
+    return file #reutrn the filename (failed) 
 
 def compile(): 
-
+  """Compiles the program"""
   if args.silent: 
     print("Silently Compiling...") 
+    #PIPE the output to silently compile.
     subprocess.run(["ant"], stdout=subprocess.PIPE, cwd=f"./Phase{args.phase}")
   else: 
     subprocess.run(["ant"], cwd=f"./Phase{args.phase}")     
@@ -150,9 +179,10 @@ def compile():
   print("Compiled.")
   
 async def main(): 
-  testsFolder = f"./unit_tests/{args.phase}/" 
-  failedFiles:list[str] = [] 
+  testsFolder = f"./unit_tests/{args.phase}/" #The unit_tests folder
+  failedFiles:list[str] = [] #Any failed files
 
+  #Potential options
   options = { 
     "plus" : ["_Plus"],
     "star": ["_Star"], 
@@ -161,12 +191,13 @@ async def main():
     "good": ["GoodTests"] 
   }
 
-  if 'filename' not in args: 
-    if 'recheck' in args and args.recheck: 
+  
+  if 'filename' not in args: #Check multiple files 
+    if 'recheck' in args and args.recheck: #Recheck the files in failed.txt
       print("Rechecking") 
       try: 
 
-        files = readData("failed.txt")
+        files = readData("failed.txt") 
         if args.compile: 
           compile() 
 
@@ -177,15 +208,17 @@ async def main():
             if ret: 
               failedFiles.append(f"{file}") 
         
-        writeData(failedFiles, "failed.txt") 
+        writeData(failedFiles, "failed.txt") #Rewrite the failed files
 
       except FileNotFoundError as err: 
         print("Unable to find failed.txt, run initial tests first.") 
         return 
+      
     else:
-      folders = [""]
-      subfolders = ["GoodTests"] 
+      folders = [""]  #Default Espresso folder
+      subfolders = ["GoodTests"] #Default GoodTests Folder
 
+      # Parse the args again... heh
       for key, value in args._get_kwargs(): 
         if key not in ['phase', 'compile', 'recheck', 'silent', 'verbose']: 
           if value: 
@@ -197,6 +230,7 @@ async def main():
       if args.compile: 
         compile() 
 
+      # Grab all the files from our parsed options (in order) and test them
       for folder in folders: 
         for subfolder in subfolders: 
           testDir = testsFolder + f'/Espresso{folder}/{subfolder}'
@@ -209,17 +243,18 @@ async def main():
       
       writeData(failedFiles, "failed.txt")
   else: 
+    #If we're testing a single file
     file = None
 
     if './unit_tests' in args.filename: 
-      file = Path(args.filename) 
+      file = Path(args.filename) #If the full filepath was provided, test the file
 
-    if not file: 
+    if not file: #Otherwise walk the unit_tests directory and find the file
       fileLoc = ""
       for root, dirs, files in os.walk(testsFolder): 
         for f in files: 
           if f.lower() == args.filename or f == args.filename: 
-            if fileLoc: 
+            if fileLoc: #We found multiple filenames
               raise RuntimeError(
                 f"Multiple file locations found for same file. Retry with exact filepath." +
                 f"\n\t{fileLoc}, or \n\t{os.path.join(root, f)}")
@@ -234,11 +269,11 @@ async def main():
 
       file = Path(fileLoc)
 
-      await test(file, True) 
+      await test(file, True) #test the file
 
 if __name__ == "__main__":
 
-  if sys.version_info.minor < 7: 
+  if sys.version_info.minor < 7:  #Asyncio does not support .run() on python < 3.7 
     loop = asyncio.get_event_loop()
     result = loop.run_until_complete(main())
   else: 
